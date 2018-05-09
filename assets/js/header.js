f7d99ee7f113da289aacd8c861046c5b4d128f9e
Ken Spencer <me@iotaspencer.me>
2018-05-09 01:24:52 -0400

    merge everything

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,17 +1,16 @@
 ---
 ---
-$(window).scroll(function() {
+
+$(window).scroll(function () {
   if ($(document).scrollTop() > 50) {
     $('nav').addClass('shrink');
   } else {
     $('nav').removeClass('shrink');
-    $('div.wrapper').css('top', 120);
+
 
   }
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
   $('[data-toggle="tooltip"]').tooltip();
-  $('div.wrapper').css('top', 120);
-
 });
